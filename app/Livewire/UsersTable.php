b56c1dfa638b48b3d60440cac2edc6b8a47071fd
Jesse-js <jesse-jjs@hotmail.com>
2024-05-23 20:13:51 -0300

    feat: adds the index of all users of tha database

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersTable extends Component
 {
+
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.users-table');
+        return view('livewire.users-table', ['users' => User::paginate(5)]);
     }
 }
