bee887547d81c557e308c84bd34787ffef4539e4
Jesse-js <jesse-jjs@hotmail.com>
2024-05-23 20:47:20 -0300

    feat: adds registers per page control for users table

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -11,8 +11,10 @@ class UsersTable extends Component
 
     use WithPagination;
 
+    public int $registersPerPage = 5;
+
     public function render()
     {
-        return view('livewire.users-table', ['users' => User::paginate(5)]);
+        return view('livewire.users-table', ['users' => User::paginate($this->registersPerPage)]);
     }
 }
--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -71,6 +71,7 @@
                         <div class="flex space-x-4 items-center mb-3">
                             <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
                             <select
+                                wire:model.live="registersPerPage"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
                                 <option value="5">5</option>
                                 <option value="10">10</option>
